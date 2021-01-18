6ef3d3edf3153eb4b9e32bd6e1caf1a8f232b54f
EC2 Default User <ec2-user@ip-172-31-28-134.ec2.internal>
2021-01-18 07:46:20 +0000

    [finish]4-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'post_images#index'
-  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-    
+  resources :post_images, only: [:new, :create, :index, :show] do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
+  # 以下の行のonly: []内にeditを追加
+  resources :users, only: [:show, :edit, :update]
 end
